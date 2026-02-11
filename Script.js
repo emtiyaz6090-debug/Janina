26753cea22a539fa9ea8ec0739150e6c1ab97f8d
emtiyaz6090-debug <emtiyaz6090@gmail.com>
2026-02-11 16:37:33 +0600

    Update Script.js

--- Script.js
+++ Script.js
@@ -59,3 +59,47 @@ btn.style.position="absolute";
 btn.style.top=Math.random()*80+"%";
 btn.style.left=Math.random()*80+"%";
 }
+/* DARK MODE TOGGLE */
+
+function toggleMode(){
+document.body.classList.toggle("dark");
+}
+
+/* SCROLL REVEAL CINEMATIC */
+
+const sections = document.querySelectorAll(".section");
+
+window.addEventListener("scroll", () => {
+sections.forEach(sec => {
+const top = sec.getBoundingClientRect().top;
+if(top < window.innerHeight - 100){
+sec.classList.add("show");
+}
+});
+});
+
+/* MULTI PAGE LOVE LETTER */
+
+let currentPage = 0;
+const pages = document.querySelectorAll(".page");
+
+function nextPage(){
+pages[currentPage].classList.remove("active");
+currentPage++;
+
+if(currentPage >= pages.length){
+currentPage = 0;
+}
+
+pages[currentPage].classList.add("active");
+}
+
+/* Glow Particle Generator */
+
+setInterval(()=>{
+let glow=document.createElement("div");
+glow.classList.add("glow");
+glow.style.left=Math.random()*100+"%";
+document.body.appendChild(glow);
+setTimeout(()=>glow.remove(),6000);
+},800);
